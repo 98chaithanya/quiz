1e5feea61ddc342294fc4ee6991071f7217e71bf
98chaithanya <chaithanyarajan998@gmail.com>
2021-09-08 09:40:49 +0530

    Initial commit

---
+++ public/QUIZ.JS
@@ -0,0 +1,56 @@
+const myQuestions = [
+    {
+      question: "who invented javascript?",
+      answers:{
+          A:"Dougalas crockford",
+          B: "Sheryl Sandberg",
+          C: "Brendan Eich"
+      },
+      correctanswer:"C"
+    },
+    {
+        question: "Which one of these is a JavaScript package manager?",
+        answers:{
+            A:"Node.js",
+            B:"TypeScript",
+            C:"npm"
+        },
+        correctanswer:"C"
+      },
+      {
+        question: "Which tool can you use to ensure code quality?",
+        answers:{
+            A: "Angular",
+            B: "jQuery",
+            C: "RequireJS",
+            D: "ESLint"
+        },
+        correctanswer:"D"
+      },
+      {
+        question:  "Which of the following is a disadvantage of using JavaScript?",
+        answers:{
+            A :"Client-side JavaScript does not allow the reading or writing of files.",
+
+            B :"JavaScript can not be used for Networking applications because there is no such support available.",
+            
+            C :"JavaScript doesn't have any multithreading or multiprocess capabilities.",
+            
+            D : "All of the above."
+        },
+        correctanswer:"D"
+      },
+      {
+        question: " How can you get the type of arguments passed to a function?",
+        answers:{
+            A:"using typeof operator",
+
+            B :"using getType function",
+            
+            C:"Both of the above",
+            
+            D:"None of the above"
+        },
+        correctanswer:"A"
+      }
+]
